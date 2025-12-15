af6d6a1356f008a152c0ae6bf4a723336a2d40ae
Sohaib SEGUENI <sohaibsegueni8@gmail.com>
2025-12-15 05:09:26 +0100

    remove unused prompt variable from exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -2,11 +2,6 @@ from dotenv import load_dotenv
 from google import genai
 import os
 
-prompt = """
-classify the user input into NEUTRAL, POSITIVE, or NEGATIVE.
-
-"""
-
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
